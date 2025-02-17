c8ea57544f7f3689f878ccaf0e0a82cb20de6a2c
Junjie Zhou <menging1122@gmail.com>
2025-02-17 19:25:05 +0800

    change the color

--- index.js
+++ index.js
@@ -2,16 +2,16 @@
 // Replace the arguments below according to your preference.
 
 // space, scary, military, romantic, cowboy, fantasy, superhero
-favouriteMovieGenre("space")
+favouriteMovieGenre("cowboy")
 
 // watermelon, tomato, banana, orange, avocado, blueberry
-favouriteFruit("avocado")
+favouriteFruit("banana")
 
 // light, dark
-favouriteMode("dark")
+favouriteMode("light")
 
 // sharp, soft, round
-favouriteEdgeStyle("soft")
+favouriteEdgeStyle("sharp")
 
 
 
